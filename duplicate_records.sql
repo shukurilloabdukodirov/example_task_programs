7998567d96b0217ed5a8244960a6f65d7dbcaff0
shukurilloabdukodirov <hi.shukurillo@gmail.com>
2024-10-02 11:02:04 +0500

    task files are ready

---
+++ duplicate_records.sql
@@ -0,0 +1,4 @@
+SELECT id
+FROM table
+GROUP BY id
+HAVING COUNT(id) > 1;
--- script.php
+++ script.php
@@ -1,1 +1,28 @@
 <?php
+function checkBracketsBalance($expression) {
+    // Closing brackets
+    $pairs = [
+        ')' => '(',
+        ']' => '[',
+        '}' => '{'
+    ];
+    $stack = [];
+
+    for ($i = 0; $i < strlen($expression); $i++) {
+        $char = $expression[$i];
+
+        // If chat is opening brackets add to stack
+        if (in_array($char, $pairs)) {
+            $stack[] = $char;
+        } elseif (isset($pairs[$char])) {  //if it is closing bracket
+            if (empty($stack) || array_pop($stack) !== $pairs[$char]) {
+                return "Неправильно";
+            }
+        }
+    }
+    return empty($stack) ? "Правильно" : "Неправильно";
+}
+
+$expression = "[({})]";
+echo checkBracketsBalance($expression); // Вывод: Правильно
+
